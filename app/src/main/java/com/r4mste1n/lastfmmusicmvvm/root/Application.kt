8ee234cb8365426325ac55ee40fb7d82fb3b36d7
ashtain <ashtain@itstudiodev.com>
2020-05-10 14:51:44 +0300

    initial commit

---
+++ app/src/main/java/com/r4mste1n/lastfmmusicmvvm/root/Application.kt
@@ -0,0 +1,19 @@
+package com.r4mste1n.lastfmmusicmvvm.root
+
+import android.app.Application
+import com.r4mste1n.lastfmmusicmvvm.BuildConfig
+import timber.log.Timber
+
+/**
+ * Created by Alex Shtain on 02.05.2020.
+ */
+class Application : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // initialise Timber
+        if (BuildConfig.DEBUG) Timber.plant(Timber.DebugTree())
+    }
+
+}
---
+++ app/src/main/java/com/r4mste1n/lastfmmusicmvvm/root/GlideModule.kt
@@ -0,0 +1,10 @@
+package com.r4mste1n.lastfmmusicmvvm.root
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+/**
+ * Created by Alex Shtain on 02.05.2020.
+ */
+@GlideModule
+class GlideModule : AppGlideModule()
